8465fe4a1bff565ede530b1f333c544932e2f615
Chris Dibbern <chrisdibbern@gmail.com>
2013-08-17 23:32:36 -0500

    Got project to use node conventions. More AST work.

---
+++ theory/lib/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var parser	= require('./theory-parser.js').parser;
+var compiler = require('./theory-compiler.js').compiler;
+var fs = require('fs');
+var path = require('path');
+
+var version = require('../package.json').version;
+
+var opts = require("nomnom")
+	.script('theory')
+	.option('file', {
+		flag: true,
+		position: 0,
+		help: '.theory source file'
+	})
+	.parse();
+
+exports.main = function() {
+    if (opts.file) {
+        var raw = fs.readFileSync(path.normalize(opts.file), 'utf8');
+        parser.yy = compiler;
+        console.log(JSON.stringify(parser.parse(raw), null, 4));
+    }
+};
+
+if (require.main === module)
+	exports.main();
