22f3f7ba0d83728f05722168c06aeba92555666e
Chris Dibbern <chrisdibbern@gmail.com>
2013-09-08 13:23:42 -0500

    Tests finally working. Case studies added.

---
+++ theory/tests/case-studies/all-studies.js
@@ -0,0 +1,20 @@
+var parser = require("../../lib/theory-parser").parser,
+	assert = require("assert"),
+	fs = require("fs"), path = require("path");
+
+// look for .theory files in this directory and add a test for each one
+
+var srcTests = {};
+var srcs = fs.readdirSync(__dirname)
+	.filter(function(filename) { return /.theory$/.test(filename); });
+
+for (var i=0, l=srcs.length; i<l; i++) {
+	srcTests["test " + srcs[i]] = (function(path) {
+		return function() {
+			var src = fs.readFileSync(path, "utf8");
+			assert.ok(parser.parse(src));
+		}
+	})(__dirname + path.sep + srcs[i]);
+}
+
+exports["test Theory source case studies"] = srcTests;
