b03514621b5ae5070ae1f14d5d51f60e346c504d
Ubuntu 12.04 <ourdev@ubuntu.(none)>
2013-09-29 17:06:12 -0500

    Misc. work on compiler/tests.

--- theory/lib/theory-compiler.js
+++ theory/lib/theory-compiler.js
@@ -1,134 +1,137 @@
 var theoryCompiler = (function(){
 	"use strict";
+	var debugMode = true;
 	
 	var options = {
-		ret : 'output'
+		ret : 'output' // or 'endstate'
 	};
 	var Compiler = function(opts) {
-		for (var k in opts) {
-			options[k] = opts[k];
-		}
-	};
-
-	var sm = new StateManager('prog', 'prog');
-	var ops = {
-		'num' : function(p1, e) { return p1; },
-		'=' : function(id, expr, e) {
-			sm.addSymbol(id, expr); // lazily eval?
-		}, 
-		'*' : function(p1, p2, e) {
-			var a = e(p1), b = e(p2);
-			if (typeof a === 'number' && typeof b === 'number') {
-				return a * b;
-			}
-		},
-		'+' : function(p1, p2, e) {
-			var a = e(p1), b = e(p2);
-			if (typeof a === 'number' && typeof b === 'number') {
-				return a + b;
-			}
-		},
+		var compiler = this;
 		
-	};
-	Compiler.prototype.compile = function(ast, scope) {
-		if (! scope) {
-			scope = sm;
-		}
-	
-		if (ast instanceof Array && ast.length > 0) {
-			if (ast[0] === 'program') {
-				var namespaces = ast.slice(1);
-				namespaces.forEach(function(ns) {
-					scope.addSymbol(ns[1], compiler.compile(ns[2]));
-				});
-				if (sm.hasEntry()) {
-					return sm.getOutput();
+		for (var k in opts) { options[k] = opts[k]; }
+		
+		var rootScope = null;
+		
+		function createNamespace(nsAST, scope) {
+			var nsscope = scope.createScope('ns', nsAST[0], nsAST[1]);
+			
+			nsAST[1].forEach(function(symbol) {
+				if (symbol.length >= 3 && symbol[0] === 'theory') {
+					var theoryScope = nsscope.addSymbol(symbol[1], createTheory(symbol.slice(1), nsscope));
+					if (symbol[1].toLowerCase() === 'main') {
+						if (! rootScope.hasEntry()) {
+							rootScope.setEntry(theoryScope);
+						} else {
+							err('Multiple main theories found.');
+						}
+					}
 				} else {
+					err('Unsupported construct, ' + symbol[0] + ', found.');
+				}
+			});
+			
+			return nsscope;	
+		}
+		
+		function createTheory(theoryAST, nsscope) {
+			var theoryScope = nsscope.createScope('theory', theoryAST[0], theoryAST[1]);
+			
+			theoryAST[1].forEach(function(def) {
+				if (def[0] === '=' || def[0] === 'ff' || def[0] === 'fn' || def[0] === '@=') {
+					theoryScope.addSymbol(def[1], null, def.slice(2), true);
+				} else if (def[0] === 'tf') {
+				// the main treefrag drives compilation
 					
 				}
-				return scope;
-			} else if (ast[0] === 'ns') {
-				var nsscope = scope.addScope('ns', ast[1]);
-				var entry = null;
-				
-				// for all items in the namespace...
-				ast[2].forEach(function(symbol) {
-					if (symbol.length >= 3 && symbol[0] === 'theory') {
-						var theoryScope = nsscope.addScope('theory', symbol[1], symbol[2]);
-						if (symbol[2] && symbol[2].toLowerCase() === 'main') {
-							if (! scope.hasEntry())
-								scope.setEntry(theoryScope);
-							} else {
-								err('Multiple main theories found.');
-							}
-						}
+			});
+			
+			return theoryScope;
+		}
+		
+		Compiler.prototype.compile = function(ast) {
+			rootScope = new StateManager('prog', 'prog');
+			
+			if (ast instanceof Array && ast.length > 0) {
+				if (ast[0] === 'program') {
+					var namespaces = ast[1];
+					namespaces.forEach(function(ns) {
+						rootScope.addSymbol(ns[1], createNamespace(ns.slice(1), rootScope));
+					});
+					
+					// if a main theory was found, go ahead and generate the CSS, else return the rootScope.
+					if (rootScope.hasEntry()) { 
+						var mainScope = rootScope.getEntry();
+						var mainAST = mainScope.getAST();
+						return evalMainTheory(mainAST, mainScope);
 					} else {
-						err('Unsupported construct, ' + symbol[0] + ', found.');
-					}
-				});
-				
-				// if a main theory was found, go ahead and generate the CSS, else return the SM.
-				if (sm.hasEntry()) { 
-					var mainScope = scope.getEntry();
-					var mainAST = mainScope.getAST();
-					return compiler.compile(mainAST, mainScope);
-				} else {
-					return scope;
+						return rootScope;
+					}		
 				}
-			} else if (ast[0] instanceof Array) {
-				ast.forEach(function(def) {
-					if (def[0] === '=' || def[0] === 'ff' || def[0] === 'fn' || def[0] === '@=') {
-						scope.addSymbol(def[1], def.slice(2));
-					} else if (def[0] === 'tf') {
-					// the main treefrag drives compilation
-						
-					}
-				});
+			}
+		};
+		
+		var ops = {
+			'num' : function(p1, e) { return p1; },
+			'=' : function(id, expr, e) {
+				rootScope.addSymbol(id, expr); // lazily eval?
+			}, 
+			'*' : function(p1, p2, e) {
+				var a = e(p1), b = e(p2);
+				if (typeof a === 'number' && typeof b === 'number') {
+					return a * b;
+				}
+			},
+			'+' : function(p1, p2, e) {
+				var a = e(p1), b = e(p2);
+				if (typeof a === 'number' && typeof b === 'number') {
+					return a + b;
+				}
+			},
+			
+		};
+		Compiler.prototype.evalExpr = function(ast) {
+			if (ast instanceof Array && ast.length > 0) {
+				if (typeof ops[ast[0]] !== 'undefined') {
+					var params = ast.slice(1).ipush(compiler.evalExpr);
+					return ops[ast[0]].apply(this, params); 
+				}
+			}
+			return ast;
+		};
+		
+		function isLiteral(p) {
+			if (p instanceof Array) {
+				if (p.length > 0) {
+					return (p[0] === 'num' || p[0] === 'str');
+				}
+				return false;
+			} else {
+				return true;
 			}
 		}
-	};
-	Compiler.prototype.evalExpr = function(ast) {
-		if (ast instanceof Array && ast.length > 0) {
-			if (typeof ops[ast[0]] !== 'undefined') {
-				var params = ast.slice(1).ipush(compiler.evalExpr);
-				return ops[ast[0]].apply(this, params); 
-			}
-		}
-		return ast;
-	};
-	
-	function isLiteral(p) {
-		if (p instanceof Array) {
-			if (p.length > 0) {
-				return (p[0] === 'num' || p[0] === 'str');
-			}
-			return false;
-		} else {
-			return true;
+		
+		function err(m) {
+			throw new Exception(m);
 		}
 	}
 	
-	function err(m) {
-		throw new Exception(m);
-	}
-	
-	function StateManager(type, name, _ast) {
+	function StateManager(type, name, _ast, parentScope) {
 		var self = this;
 		var stack = [];
 		var entry = null;
 		var output = "";
 			
 		StateManager.prototype.undefined = 'undefined';
-		StateManager.prototype.addScope = function(type, name, ast) {
-			var scope = new StateManager(type, name, ast);
-			stack.push(scope);
+		StateManager.prototype.createScope = function(type, name, ast) {
+			var scope = new StateManager(type, name, ast, this);
 			return scope;
 		};
-		StateManager.prototype.addSymbol = function(id, val) {
-			stack[stack.length-1][id] = val;
+		StateManager.prototype.addSymbol = function(id, val, ast, lazy) {
+			this[id] = { val : val, ast : ast, lazy : lazy };
 		};
-		StateManager.prototype.dereference = function(id) {
+		StateManager.prototype.resolve = function(id) {
 			for (var i = stack.length - 1; i >= 0; i--) {
+				console.log(stack[i]);
 				if (typeof stack[i][id] !== 'undefined') {
 					return stack[i][id];
 				}
@@ -140,6 +143,7 @@ var theoryCompiler = (function(){
 		StateManager.prototype.getEntry = function() { return self.entry; };
 		StateManager.prototype.getAST = function() { return _ast; };
 		StateManager.prototype.getOutput = function() { return output; };		
+		StateManager.prototype.getParentScope = function() {  return parentScope; };
 	}
 
 	['push', 'unshift', 'reverse', 'splice'].forEach(function(x){
@@ -150,6 +154,8 @@ var theoryCompiler = (function(){
 	        return na;
 	    }
 	});
+	
+	function debug() { if (debugMode) console.log.apply(this, arguments); }
 
 	return Compiler;
 })();
--- theory/tests/compiler/expressions.js
+++ theory/tests/compiler/expressions.js
@@ -1,5 +1,5 @@
 var assert = require("assert"),
-	compiler = require("../../lib/theory-compiler").Compiler,
+	Compiler = require("../../lib/theory-compiler").Compiler,
 	parser = require("../../lib/theory-parser").parser;
 
 var exprTests = {};
@@ -7,6 +7,7 @@ exprTests["test basic arithmetic"] = function() {
 	/* (5 + 3) * (2 + 0x16) */
 	var expr = ['*', ['+', ['num', 5], ['num', 3]], ['+', ['num', 2], ['num', 22]]];
 
+	var compiler = new Compiler();
 	var answer = compiler.evalExpr(expr);
 
 	assert.equal(answer, 192);
@@ -19,7 +20,9 @@ exprTests["test integration math expression"] = function() {
 		+ "    expr = (5 + 3) * (2 + 0x16);\n\n";
 		
 	var ast = parser.parse(src);
-	console.log("%j", ast);
+	var compiler = new Compiler();
+	var result = compiler.compile(ast);
+	console.log("%j", result.resolve("Website"));
 }
 
 exports["test Theory compiler expressions"] = exprTests;
