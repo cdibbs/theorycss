cd3a69d38e643a666454b01b43b6d1689fa0bad9
Chris Dibbern <chrisdibbern@gmail.com>
2013-10-05 18:53:33 -0500

    Work on migrating to vows testing framework, as well as a little work on Expressions.

--- theory/src/compiler/expressions.js
+++ theory/src/compiler/expressions.js
@@ -3,10 +3,10 @@
 var Expressions = function Expressions() {
 	var self = this;
 	
-	self.evaluate = function(ast) {
+	self.evaluate = function(ast, scope) {
 		if (ast instanceof Array && ast.length > 0) {
 			if (typeof self.ops[ast[0]] !== 'undefined') {
-				var params = ast.slice(1).ipush(self.evaluate);
+				var params = ast.slice(1).ipush(self.evaluate, scope);
 				return self.ops[ast[0]].apply(this, params); 
 			}
 		}
@@ -26,31 +26,41 @@ var Expressions = function Expressions() {
 	
 	self.ops = {
 		'num' : function(p1, e) { return p1; },
-		'=' : function(id, expr, e) {
-			rootScope.addSymbol(id, expr); // lazily eval?
+		'id' : function(id, e, scope) {
+			var val = scope.resolve(id);
+			if (val.val == null && val.lazy)
+				return e(val.ast[0], scope);
+			else
+				return val.val;
+		},
+		'()' : function(id, params, e, scope) {
+			
+		},
+		'=' : function(id, expr, e, scope) {
+			scope.addSymbol(id, expr); // lazily eval?
 		}, 
-		'*' : function(p1, p2, e) {
-			var a = e(p1), b = e(p2);
+		'*' : function(p1, p2, e, scope) {
+			var a = e(p1, scope), b = e(p2, scope);
 			if (typeof a === 'number' && typeof b === 'number') {
 				return a * b;
 			}
 		},
-		'/' : function(p1, p2, e) {
-			var a = e(p1), b = e(p2);
+		'/' : function(p1, p2, e, scope) {
+			var a = e(p1, scope), b = e(p2, scope);
 			if (typeof a === 'number' && typeof b === 'number') {
 				return a / b;
 			}
 		},
-		'+' : function(p1, p2, e) {
-			var a = e(p1), b = e(p2);
+		'+' : function(p1, p2, e, scope) {
+			var a = e(p1, scope), b = e(p2, scope);
 			if (typeof a === 'number' && typeof b === 'number') {
 				return a + b;
 			}
 		},
-		'-' : function(p1, p2, e) {
-			var a = e(p1), b = e(p2);
+		'-' : function(p1, p2, e, scope) {
+			var a = e(p1, scope), b = e(p2, scope);
 			if (typeof a === 'number' && typeof b === 'number') {
-				return a + b;
+				return a - b;
 			}
 		}
 	};
--- theory/tests/compiler/expressions.js
+++ theory/tests/compiler/expressions.js
@@ -1,10 +1,91 @@
 var assert = require("assert"),
+	Expressions = require("../../src/compiler/expressions").Expressions,
 	Compiler = require("../../lib/theory-compiler").Compiler,
-	parser = require("../../lib/theory-parser").parser;
+	parser = require("../../lib/theory-parser").parser
+	vows = require("vows");
+	
 
 var exprTests = {};
-exprTests["test basic arithmetic"] = function() {
-	/* (5 + 3) * (2 + 0x16) */
+var src =
+	"namespace Website\n"
+	+ "  theory Main\n"
+	+ "    a = 0x12345;\n"
+	+ "    a'= a;\n"
+	+ "    b = 17 + 732;\n"
+	+ "    c = b + 28;\n"
+	+ "    d = (1/2) * ((50 + 30) * (20 + 0x160));\n"
+	+ "    e = 1 + 5 * 3 / 2 - 8 * 9 + 14;\n"
+	+ "    e' = 1 + 5 * 3 / 2 - 8 * 9 * c + 14 - e;\n"
+	+ "    f = basicFn();\n"
+	+ "    fn basicFn() -> 1771;\n"
+	+ "\n\n";
+var ast = new Compiler().compile(parser.parse(src))
+	.resolve("Website").val
+	.resolve("Main").val;
+		
+vows.describe("Expressions class").addBatch({
+	'evaluating' : {
+		topic : new Expressions(),
+		
+		'one constant' : {
+			topic : function(expr) { return expr.evaluate(ast.resolve("a").ast[0]); },
+			 
+			'we get that constant' : function(topic) {
+				assert.equal(topic, 0x12345);
+			}
+		},
+		'a variable look-up' : {
+			topic : function(expr) { return expr.evaluate(ast.resolve("a'").ast[0], ast); },
+			
+			'we get the contents of that variable' : function(topic) {
+				assert.equal(topic, 0x12345);
+			}
+		},
+		'two-term addition of constants' : {
+			topic : function(expr) { return expr.evaluate(ast.resolve("b").ast[0]); },
+			
+			'we get the sum of the two terms' : function(topic) {
+				assert.equal(topic, 749);
+			}
+		},
+		'two-term addition, requiring a look-up' : {
+			topic : function(expr) { return expr.evaluate(ast.resolve("c").ast[0], ast); },
+			
+			'we get the sum of the two terms' : function(topic) {
+				assert.equal(topic, 777);
+			}
+		},
+		'complex arithmetic involving constants' : {
+			topic : function(expr) { return expr.evaluate(ast.resolve("d").ast[0], ast); },
+			
+			'we get the correct result' : function(topic) {
+				assert.equal(topic, 14880);
+			}
+		},
+		'arithmetic requiring proper order of ops' : {
+			topic : function(expr) { return expr.evaluate(ast.resolve("e").ast[0], ast); },
+			
+			'we get the sum of the two terms' : function(topic) {
+				assert.equal(topic, -49.5);
+			}
+		},
+		'arithmetic requiring proper order of ops and look-ups' : {
+			topic : function(expr) { return expr.evaluate(ast.resolve("e'").ast[0], ast); },
+			
+			'we get the sum of the two terms' : function(topic) {
+				assert.equal(topic, -55872);
+			}
+		},
+		'a simple function call' : {
+			topic : function(expr) { return expr.evaluate(ast.resolve("f").ast[0], ast); },
+			
+			'we get the result of the function' : function(topic) {
+				assert.equal(topic, 1771);
+			}
+		},
+	}
+}).export(module);
+/*exprTests["test basic arithmetic"] = function() {
 	var expr = ['*', ['+', ['num', 5], ['num', 3]], ['+', ['num', 2], ['num', 22]]];
 
 	var compiler = new Compiler();
@@ -18,7 +99,7 @@ exprTests["test integration simple math expressions"] = function() {
 		"namespace Website\n"
 		+ "  theory Main\n"
 		+ "    a = (1/2) * ((50 + 30) * (20 + 0x160));\n"
-		+ "    b = 1 + 5 * 3 / 2 - 8 * 90 + 1480;\n\n";
+		+ "    b = 1 + (((5 * 3) / 2) - (8 * 90)) + 1480;\n\n";
 		
 	var ast = parser.parse(src);
 	var compiler = new Compiler();
@@ -29,10 +110,10 @@ exprTests["test integration simple math expressions"] = function() {
 	var answer_b = compiler.evaluateExpression(exprASTs.resolve("b").ast[0]);
 	
 	assert.equal(answer_a, 14880);
-	assert.equal(answer_b, 14880);
+	assert.equal(answer_b, 2208.5);
 };
 
-exports["test Theory compiler expressions"] = exprTests;
+exports["test Theory compiler expressions"] = exprTests; 
 
 if (require.main === module)
-	require("test").run(exports);
+	require("test").run(exports); */
